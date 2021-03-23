7b02388610655e2c61cce87e0c5bb1dbce672116
curoli <curoli@gmail.com>
2021-03-23 10:46:44 +0100

    Command line parsing, part 1.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate clap;
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world!");
+    let cli_config =
+        App::new("Megadrile")
+            .version("0.1.0")
+            .author("Oliver Ruebenacker <oliverr@broadinstitute.org>")
+            .about("Crunching genomic data")
+            .arg(Arg::with_name("input")
+                .short("i")
+                .long("input")
+                .value_name("INPUT")
+                .help("The input file")
+                .takes_value(true))
+            .get_matches();
+    match cli_config.value_of("input") {
+        Some(input) => println!("Input: {}", input),
+        None => println!("No input specified.")
+    }
+    println!("Done!");
 }
