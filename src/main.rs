835b762b8838f8b10d007bcfa490720ab5e455cc
curoli <curoli@gmail.com>
2021-03-24 17:33:53 +0100

    Very minor refactor.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
 extern crate clap;
-use clap::{Arg, App};
+use clap::{Arg, App, ArgMatches};
+
+fn get_cli_config() -> ArgMatches {
+    App::new("Megadrile")
+        .version("0.1.0")
+        .author("Oliver Ruebenacker <oliverr@broadinstitute.org>")
+        .about("Crunching genomic data")
+        .arg(Arg::with_name("input")
+            .short("i")
+            .long("input")
+            .value_name("INPUT")
+            .help("The input file")
+            .takes_value(true))
+        .get_matches()
+}
 
 fn main() {
-    let cli_config =
-        App::new("Megadrile")
-            .version("0.1.0")
-            .author("Oliver Ruebenacker <oliverr@broadinstitute.org>")
-            .about("Crunching genomic data")
-            .arg(Arg::with_name("input")
-                .short("i")
-                .long("input")
-                .value_name("INPUT")
-                .help("The input file")
-                .takes_value(true))
-            .get_matches();
+    let cli_config = get_cli_config();
     match cli_config.value_of("input") {
         Some(input) => println!("Input: {}", input),
         None => println!("No input specified.")
