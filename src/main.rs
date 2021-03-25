4426ebde2ac0100a6be6b9a471dac3c3b77ec24d
curoli <curoli@gmail.com>
2021-03-25 22:53:41 +0100

    Using vcf crate, part 1

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
+mod config;
+
 extern crate clap;
-use megadrile::get_cli_config;
+use std::fs::File;
 
 fn main() {
-    let cli_config = get_cli_config();
+    let cli_config = config::get_cli_config();
     match cli_config.value_of("input") {
-        Some(input) => println!("Input: {}", input),
-        None => println!("No input specified.")
+        Some(input) => {
+            let result = File::open(input);
+            match result {
+                Ok(_) => println!("Got file"),
+                Err(err) => println!("Got error trying to open file: {}", err)
+            }
+            println!("Input: {}", input)
+        },
+        None =>
+            println!("No input specified.")
     }
     println!("Done!");
 }
