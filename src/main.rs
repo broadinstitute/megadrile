119cb0fbc2a40097100133bed7dd2e2ce1b5f310
curoli <curoli@gmail.com>
2021-03-29 20:43:05 -0400

    Reading header and records.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod config;
 
 extern crate clap;
 use std::fs::File;
-use vcf::{VCFReader, VCFError};
+use vcf::{VCFReader, VCFError, VCFRecord};
 use std::io::{BufReader};
 use std::io;
 use flate2::read::MultiGzDecoder;
@@ -33,8 +33,36 @@ fn main() {
     let cli_config = config::get_cli_config();
     match cli_config.value_of("input") {
         Some(input) => {
-            let mut vcf_reader = get_vcf_reader(input);
-            println!("Input: {}", input)
+            println!("Input: {}", input);
+            let vcf_reader = get_vcf_reader(input);
+            match vcf_reader {
+                Ok(mut vcf_reader) => {
+                    let header = vcf_reader.header();
+                    let n_samples = header.samples().len();
+                    println!("Number of samples: {}", n_samples);
+                    let mut record: VCFRecord = vcf_reader.empty_record();
+                    let mut n_records = 0;
+                    loop {
+                        match vcf_reader.next_record(&mut record) {
+                            Ok(got_record) => {
+                                if got_record {
+                                    n_records += 1;
+                                } else {
+                                    break
+                                }
+                            }
+                            Err(_) => {
+                                println!("Something went wrong while reading record.");
+                                break
+                            }
+                        }
+                    }
+                    println!("Number of records: {}", n_records);
+                }
+                Err(_snag) => {
+                    println!("Something went wrong!")
+                }
+            }
         },
         None =>
             println!("No input specified.")
