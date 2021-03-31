fccb6ff706d737939beb45a58c0dc127b9430a90
curoli <curoli@gmail.com>
2021-03-31 17:00:31 -0400

    Abstracting iterating over VCF records.

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,10 @@
+use vcf::VCFRecord;
+
 pub mod config;
 pub mod error;
+
+pub trait VcfRecordInspector<R> {
+    fn reset(&mut self) -> ();
+    fn inspect_record(&mut self, record: &VCFRecord) -> ();
+    fn get_result(&self) -> R;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,44 @@
 extern crate clap;
+
 use std::fs::File;
 use vcf::{VCFReader, VCFRecord};
 use std::io::{BufReader};
 use flate2::read::MultiGzDecoder;
-use megadrile::{error, config};
+use megadrile::{error, config, VcfRecordInspector};
 
 fn get_vcf_reader(input: &str)
-    -> Result<VCFReader<BufReader<MultiGzDecoder<File>>>, error::Error> {
-    Ok(VCFReader::new(BufReader::new(MultiGzDecoder::new(File::open(input)?)))?)
+                  -> Result<VCFReader<BufReader<MultiGzDecoder<File>>>, error::Error> {
+    Ok(
+        VCFReader::new(
+            BufReader::new(MultiGzDecoder::new(File::open(input)?))
+        )?
+    )
+}
+
+struct RecordCounter {
+    n_records: u32
+}
+
+impl RecordCounter {
+    fn new() -> RecordCounter {
+        RecordCounter {
+            n_records: 0
+        }
+    }
+}
+
+impl VcfRecordInspector<u32> for RecordCounter {
+    fn reset(&mut self) -> () {
+        self.n_records = 0;
+    }
+
+    fn inspect_record(&mut self, _record: &VCFRecord) -> () {
+        self.n_records += 1;
+    }
+
+    fn get_result(&self) -> u32 {
+        self.n_records
+    }
 }
 
 fn main() {
@@ -22,29 +53,29 @@ fn main() {
                     let n_samples = header.samples().len();
                     println!("Number of samples: {}", n_samples);
                     let mut record: VCFRecord = vcf_reader.empty_record();
-                    let mut n_records = 0;
+                    let mut record_counter = RecordCounter::new();
                     loop {
                         match vcf_reader.next_record(&mut record) {
                             Ok(got_record) => {
                                 if got_record {
-                                    n_records += 1;
+                                    record_counter.inspect_record(&record);
                                 } else {
-                                    break
+                                    break;
                                 }
                             }
                             Err(_) => {
                                 println!("Something went wrong while reading record.");
-                                break
+                                break;
                             }
                         }
                     }
-                    println!("Number of records: {}", n_records);
+                    println!("Number of records: {}", record_counter.get_result());
                 }
                 Err(_error) => {
                     println!("Something went wrong!")
                 }
             }
-        },
+        }
         None =>
             println!("No input specified.")
     }
